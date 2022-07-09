f57a21051ade7e79ff75a8680290f8fdbdfda99a
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-07-08 23:48:51 -0300

    Navbar is added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,11 +3,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { LogiPage } from "../auth/pages/LogiPage";
 import { BlackBeerPage } from "../beers/pages/BlackBeerPage";
 import { BlondBeerPage } from "../beers/pages/BlondBeerPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
-      {" "}
+      <Navbar />
       <Routes>
         <Route path="blondbeer" element={<BlondBeerPage />} />
         <Route path="blackbeer" element={<BlackBeerPage />} />
