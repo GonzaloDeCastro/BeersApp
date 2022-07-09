55939f65f5b4bfd844e72db97d7fba18507cb8e0
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-07-09 00:23:32 -0300

    Login and Logout are workin

--- src/auth/pages/LogiPage.jsx
+++ src/auth/pages/LogiPage.jsx
@@ -1,3 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 export const LogiPage = () => {
-  return <h1>LogiPage</h1>;
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/blondbeer", {
+      replace: true,
+    });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
